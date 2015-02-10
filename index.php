034ab9891e45d559276e74d3509b070f717deea4
mashpy <mashpysays@gmail.com>
2015-02-10 09:46:22 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="template.css">
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script></head>
+
+<body>
+     <header class="header_area"> 
+  <div class="container">
+
+    <div class="row">      
+      <div class="col-lg-6">
+           <img class="img-responsive" src="images/logo.png">
+        Institute Canada
+      </div>
+      
+      <div class="col-lg-6">
+        <div class="col-lg-5"> <!--Phone Start-->
+          <div class="phone"> 1-888-244-1106 </div>
+          <div class="email">info@uni-prep.com</div>
+        </div> <!--Phone End-->
+
+        <div class="col-lg-7"> <!--Social Start-->
+          
+<div class="socbuttons"><a href="mailto:info@uni-prep.com">															</a><a href="http://www.facebook.com/UNIprepInstitute" target="_blank" class="float-shadow"><img src="/images/socials/facebook.jpg" alt="facebook" style="margin-right: 5px; float: left;"></a>															<a href="https://www.youtube.com/channel/UCgb8bDILtewGil-ZSnPrqHA" target="_blank" class="float-shadow"><img src="/images/socials/youtube.jpg" alt="youtube" style="margin-right: 5px; float: left;"></a>															<a href="http://www.twitter.com/UNIPrepTraining" target="_blank" class="float-shadow"><img src="/images/socials/twitter.jpg" alt="tweter" style="margin-right: 5px; float: left;"></a>    														<a href="http://www.linkedin.com/company/uni-prep-corporation" target="_blank" class="float-shadow"><img src="/images/socials/linkedin.jpg" alt="linkedin" style="margin-right: 5px; float: left;"></a>    														<a href="https://plus.google.com/+UniprepInstitute" target="_blank" class="float-shadow"><img src="/images/socials/googleplus.png" alt="googleplus" style="margin-right: 5px; float: left;"></a>															</div>  
+          
+        </div> <!--Social End--> 
+          
+      </div> <!--End of col-lg-6 -->
+      
+    </div> <!--End of row -->
+  </div> <!--end of container-->
+</header> <!--end of header area-->
+  
+  <div class="nav_area_total">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-9 col-sm-12 main-nav-area">
+						<nav class="menu">
+							<ul>
+								<li><a href="">Home</a></li>
+								<li><a href="">About us</a></li>
+								<li><a href="">Courses</a>
+									<ul>
+										<li><a href="">Home</a></li>
+										<li><a href="">About us</a></li>
+										<li><a href="">Courses</a></li>
+										<li><a href="">Corporate training</a></li>
+										<li><a href="">Contact</a></li>
+										<li><a href="">Blog</a></li>
+										<li><a href="">WP</a></li>
+									</ul>
+								</li>
+								<li><a href="">Corporate training</a></li>
+								<li><a href="">Contact</a></li>
+								<li><a href="">Blog</a></li>
+								<li><a href="">WP</a></li>
+							</ul>
+						</nav> <!--End of Nav-->
+					</div>
+				</div>
+			</div>
+		</div> <!--nav_area_total End-->
+  
+</body>  
+
+</html>
